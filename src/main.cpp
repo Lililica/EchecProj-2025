7982eda8d36415dca09da19b3780fd5d94e28642
Lililica <lucas.terrasson@gmail.com>
2025-04-05 12:34:19 +0200

    Coucou rien n'a changé

--- src/main.cpp
+++ src/main.cpp
@@ -40,6 +40,7 @@ int main(int argc, char** argv)
     mainApp.getRender3D()->init_mat_proj();
 
     /* ________________________________________________________________________ */
+    // /* ImGui init */
 
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO();
